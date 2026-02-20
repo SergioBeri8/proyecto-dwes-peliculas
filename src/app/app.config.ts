062069aa2ff4a80a3cb48172e7f85e15fbd7497b
SergioBeri8 <sergio.beriguete@educa.madrid.org>
2026-02-20 13:52:40 +0100

    Fix: Conflictos resueltos manualmente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-=======
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
->>>>>>> temp-fix
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
-};
-=======
     provideRouter(routes),
     provideHttpClient()
   ]
 };
->>>>>>> temp-fix
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,9 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-
-export const routes: Routes = [];
-=======
 import { LoginComponent } from './features/auth/pages/login/login.component';
 
 export const routes: Routes = [
-  // 1. Si no ponemos nada, que vaya al login
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  
-  // 2. Ruta para el componente de Login
   { path: 'login', component: LoginComponent },
-  
-  // 3. Ruta para el Dashboard (por ahora lo mandamos al login hasta que lo creemos)
   { path: 'dashboard', component: LoginComponent },
-
-  // 4. Comodín: Cualquier cosa rara, al login
   { path: '**', redirectTo: '/login' }
 ];
->>>>>>> temp-fix
